b3eeeb61278ab5e127100c88c98230e3c7581751
AmanSRawat <amamrawat96@gmail.com>
2026-03-17 22:52:31 +0530

    initilized the medical chatbot project

---
+++ Medical Chatbot/template.sh
@@ -0,0 +1,12 @@
+mkdir -p src
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompts.py
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+
+
+echo "created directories and files for the project"
